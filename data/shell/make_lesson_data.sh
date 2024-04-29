3dab293a2b8c2b7889450ae4a84c07d34d0e903c
Peter van Heusden <pvanheusden@uwc.ac.za>
2024-04-29 10:57:34 +0200

    Add more files to the list to download

--- data/shell/make_lesson_data.sh
+++ data/shell/make_lesson_data.sh
@@ -6,6 +6,12 @@ if [ ! -d $HOME/data ] ; then
 	mkdir $HOME/data
 fi
 
-cd $HOME/data && wget -c https://pathogen-genomics-march-2024.sanbi.ac.za/data/shell/cases.csv
+BASE_URL="https://pathogen-genomics-march-2024.sanbi.ac.za/data/shell"
+cd $HOME/data
+
+for FILENAME in cases.csv file_list1.csv file_list2.csv ; do 
+	wget -c $BASE_URL/$FILENAME
+done 
+
 echo "DONE"
 
