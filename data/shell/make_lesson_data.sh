b7d8a513939ccc46954056c25b616efa6a311dbf
Peter van Heusden <pvanheusden@uwc.ac.za>
2024-04-29 11:01:03 +0200

    Switch to using curl for downloads

--- data/shell/make_lesson_data.sh
+++ data/shell/make_lesson_data.sh
@@ -10,7 +10,7 @@ BASE_URL="https://pathogen-genomics-march-2024.sanbi.ac.za/data/shell"
 cd $HOME/data
 
 for FILENAME in cases.csv file_list1.csv file_list2.csv ; do 
-	wget -c $BASE_URL/$FILENAME
+	curl $BASE_URL/$FILENAME >$HOME/data/$FILENAME
 done 
 
 echo "DONE"
